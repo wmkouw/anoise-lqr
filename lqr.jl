2897f0048c61c0469e034d2cb29f1b6c0115ccb4
wmkouw <wmkouw@gmail.com>
2022-10-03 14:57:59 +0200

    Switched back to Plots. Added measurement noise.

--- lqr.jl
+++ lqr.jl
@@ -1,6 +1,11 @@
 using Revise
 using LinearAlgebra
-using GLMakie
+using Distributions
+using Rocket
+using GraphPPL
+using ReactiveMP
+using Plots
+default(label="", margin=10Plots.pt)
 
 # Define system structure
 mutable struct PositioningSystem
@@ -11,22 +16,28 @@ mutable struct PositioningSystem
     # Load
     mass :: Float64
 
-    PositioningSystem(state, mass) = new(state, mass)
+    # Measurement noise variance
+    mnoise_var :: Float64
+
+    PositioningSystem(state, mass, mnoise_var) = new(state, mass, mnoise_var)
 end
 
 # Mass of object
 mass = 2.0 # kg
 
+# Measurement noise
+mnoise_var = 0.01
+
 # Time scales
 Δt = 0.01   # seconds
-T = 3       # seconds
+T = 1       # seconds
 timesteps = range(0, T, step=Δt)
 
 # Define system in initial state
-system = PositioningSystem(zeros(6), mass)
+system = PositioningSystem(zeros(6), mass, mnoise_var)
 
 # System dynamics
-function move!(system::PositioningSystem, force::Vector{Float64}; Δt::Float64 = 1.0)
+function move!(system::PositioningSystem, force::Vector{Float64}; σ::Float64 = 1.0, Δt::Float64 = 1.0)
 
     # State transition matrix    
     A = [I(3)     Δt*I(3); 
@@ -35,30 +46,24 @@ function move!(system::PositioningSystem, force::Vector{Float64}; Δt::Float64 =
     # Control matrix
     B = [zeros(3,3); I(3)*Δt/system.mass] 
 
+    # Observation matrix
+    C = [I(3) zeros(3,3)]
+
+    # Update system state
     system.state = A*system.state + B*force
+
+    # Generate observations
+    observations = C*system.state + system.mnoise_var*randn(3)
 end
 
 # Generate force
 force = cat([[cos.(k/T*2π), sin.(k/T*2π), 0.0] for k in timesteps]..., dims=2)
 
 # Hold states
-states = cat([move!(system, force[:,k], Δt=Δt) for (k,t) in enumerate(timesteps)]..., dims=2)
+sensor = cat([move!(system, force[:,k], Δt=Δt) for (k,t) in enumerate(timesteps)]..., dims=2)
 
-# Init figure
-fig = Figure(; resolution=(1200, 600))
-ax1 = Axis(fig[1, 1])
-ax2 = Axis(fig[1, 2])
-ax3 = Axis(fig[1, 3])
-ax4 = Axis(fig[2, 1])
-ax5 = Axis(fig[2, 2])
-ax6 = Axis(fig[2, 3])
-
-# Plot forces
-plot!(ax1, timesteps, force[1,:], label="force x")
-plot!(ax2, timesteps, force[2,:], label="force y")
-plot!(ax3, timesteps, force[3,:], label="force z")
-
-# Plot states
-plot!(ax4, timesteps, states[1,:], label="position x")
-plot!(ax5, timesteps, states[2,:], label="position y")
-plot!(ax6, timesteps, states[3,:], label="position z")
+# Visualize
+p101 = plot(timesteps, force[1,:], label="force x")
+p102 = plot(timesteps, force[2,:], label="force y")
+p201 = scatter(sensor[1,:], sensor[2,:], xlabel="x coordinate", ylabel="y coordinate")
+plot(p101, p102, p201, layout=(3,1), size=(500,1000))
